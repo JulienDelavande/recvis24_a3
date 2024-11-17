2816786fe72dc611263519fd10a13670812266ed
Julien Delavande <Julien.DELAVANDE@student.isae-supaero.fr>
2024-11-18 00:22:37 +0100

    efnetv2m

--- model_factory.py
+++ model_factory.py
@@ -48,8 +48,8 @@ class ModelFactory:
             return data_transforms_224
         if self.model_name == "resnet101":
             if self.test_mode:
-                return data_transforms_224
-            return data_transforms_224
+                return data_transforms_384
+            return data_transforms_384
         if self.model_name.startswith("efficientnet_b"):
             if self.test_mode:
                 return data_transforms_224
